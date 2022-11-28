29d5b95d29cacd28fd69d3775049673cd28f13b8
“ptradic” <pradic16@gmail.com>
2022-11-28 22:33:30 +0100

    update

--- Project1/Project1/Source.cpp
+++
@@ -1,6 +0,0 @@
-#include <stdio.h>
-
-int main(void) {
-	printf("Kiki ima velika jaja");
-	return 0;
-}
--- ProjectP/ProjectP/Source.c
+++ ProjectP/ProjectP/Source.c
@@ -13,29 +13,54 @@ struct Cvor {
 
 int UnosP(int x, Pozicija P);
 int UnosK(int x, Pozicija P);
+int UnosSort(int x, Pozicija P);
+
 int Ispis(Pozicija P);
+
 Pozicija Trazi(int x, Pozicija P);
+Pozicija TraziPret(int x, Pozicija P);
+
 int Brisi(int x, Pozicija P);
+int BrisiMin(Pozicija P);
 int BrisiSve(Pozicija P);
+int BrisiSveMin(Pozicija P);
 
 int main() {
 	struct Cvor Head;
 	Head.Next = NULL;
 	Pozicija P;
+	int T1, T2;
 
 	UnosK(1, &Head);
 	UnosP(3, &Head);
 	UnosP(8, &Head);
 	UnosP(2, &Head);
 	UnosK(5, &Head);
+
 	Ispis(Head.Next);
-	Trazi(5, Head.Next);
-	printf("\nElement 5 nalazi se na %p adresi.\n", &P);
-	Trazi(1, Head.Next);
-	printf("\nElement 1 nalazi se na %p adresi.\n", &P);
+
+	T1=Trazi(5, Head.Next);
+	printf("\nElement 5 nalazi se na %p adresi.\n", &T1);
+
+	T2=TraziPret(1, Head.Next);
+	printf("\nPrethodni element elementa 1 nalazi se na %p adresi.\n", &T2);
+
 	Brisi(8, &Head);
+
 	Ispis(Head.Next);
-	BrisiSve(Head.Next);
+
+	printf("\n");
+
+	BrisiSve(&Head);
+
+	UnosSort(5, &Head);
+	UnosSort(2, &Head);
+	UnosSort(3, &Head);
+	UnosSort(6, &Head);
+
+	Ispis(Head.Next);
+
+	BrisiSveMin(&Head);
 
 	return EXIT_SUCCESS;
 }
@@ -43,18 +68,22 @@ int main() {
 int UnosP(int x, Pozicija P) {
 	Pozicija Q;
 	Q = (Pozicija)malloc(sizeof(struct Cvor));
+
 	Q->El = x;
 	Q->Next = P->Next;
 	P->Next = Q;
 
 	return EXIT_SUCCESS;
 }
+
 int UnosK(int x, Pozicija P) {
 	Pozicija Q;
+	Q = (Pozicija)malloc(sizeof(struct Cvor));
+
 	while (P->Next != NULL) {
 		P = P->Next;
 	}
-	Q = (Pozicija)malloc(sizeof(struct Cvor));
+
 	Q->El = x;
 	P->Next = Q;
 	Q->Next = NULL;
@@ -63,6 +92,7 @@ int UnosK(int x, Pozicija P) {
 }
 
 int Ispis(Pozicija P) {
+
 	while (P != NULL) {
 		printf(" %d", P->El);
 		P = P->Next;
@@ -71,14 +101,45 @@ int Ispis(Pozicija P) {
 }
 
 Pozicija Trazi(int x, Pozicija P) {
+
 	while (P != NULL && P->El != x) {
 		P = P->Next;
 	}
 	return P;
 }
 
+Pozicija TraziPret(int x, Pozicija P) {
+	Pozicija Prev;
+
+	Prev = P;
+	P = P->Next;
+
+	while (P != NULL && P->El != x) {
+		Prev = Prev->Next;
+		P = P->Next;
+	}
+
+	return Prev;
+}
+
+int UnosSort(int x, Pozicija P) {
+	Pozicija Q;
+	Q = (Pozicija)malloc(sizeof(struct Cvor));
+
+	while (P->Next != NULL && P->Next->El < x) {
+		P = P->Next;
+	}
+
+	Q->El = x;
+	Q->Next = P->Next;
+	P->Next = Q;
+
+	return EXIT_SUCCESS;
+}
+
 int Brisi(int x, Pozicija P) {
-	Pozicija prev, temp;
+	Pozicija temp = NULL;
+
 	while (P->Next != NULL && P->Next->El != x){
 		P = P->Next;
 	}
@@ -94,13 +155,39 @@ int Brisi(int x, Pozicija P) {
 	return EXIT_SUCCESS;
 }
 
+int BrisiMin(Pozicija P) {
+	Pozicija min = P->Next;
+	Pozicija prevMin = P;
+
+	while (P->Next != NULL) {
+		if (P->Next->El < min->El) {
+			prevMin = P;
+			min = P->Next;
+		}
+		P = P->Next;
+	}
+	prevMin->Next = min->Next;
+	free(min);
+
+	return EXIT_SUCCESS;
+}
+
 int BrisiSve(Pozicija P) {
 	Pozicija temp;
-	while (P->Next != NULL)										//ciscenje alocirane memorije na kraju
+
+	while (P->Next != NULL)
 	{
 		temp = P->Next;
 		P->Next = temp->Next;
 		free(temp);
 	}
+	return EXIT_SUCCESS;
+}
+
+int BrisiSveMin(Pozicija P) {
+
+	while (P->Next != NULL)
+		BrisiMin(P);
+
 	return EXIT_SUCCESS;
 }
